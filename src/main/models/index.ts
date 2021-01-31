e12318f157da1798e1ac4f0151f192f5d4adbdb8
Buu97 <tokyratefinanahary97@gmail.com>
2021-01-31 12:57:15 +0300

    reset

--- src/main/models/index.ts
+++
@@ -1,5 +0,0 @@
-import { Task } from './Task';
-
-export {
-    Task
-}
--- src/renderer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
-
-const mainElement = document.createElement('div');
-document.body.appendChild(mainElement);
-
-ReactDom.render(<App />, mainElement);
